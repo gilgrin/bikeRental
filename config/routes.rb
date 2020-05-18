2d78d5896a9b2adc3815b4d44df541f844ed6617
ran cohen <ranco.nyc@gmail.com>
2020-05-18 19:53:01 +0300

    log in/out, bikes index/show

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/home', to: 'dashboards#index'
-  get '/bikes', to: 'bikes#index'
-  get '/bikes/:id', to: 'bikes#show'
-  get '/bikes/new', to: 'bikes#new'
-  post '/bikes', to: 'bikes#show'
+  resources :bikes, only: [:index, :show, :new, :create] do
+
+  end
 end
