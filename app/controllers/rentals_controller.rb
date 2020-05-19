8c59552bc27ea892ff3f563b4bea0bde15411a20
Gil Grin <gilgi@mta.ac.il>
2020-05-19 14:42:17 +0300

    only images left

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,4 +1,19 @@
 class RentalsController < ApplicationController
   def create
+    @rental = Rental.new(rental_params)
+    @bike = Bike.find(params[:bike_id])
+    @rental.bike = @bike
+    @rental.user = current_user
+    if @rental.save
+      redirect_to root_path, notice: 'Rental was successfully created.'
+    else
+      render "bikes/show"
+    end
+  end
+
+  private
+
+  def rental_params
+    params.require(:rental).permit(:date)
   end
 end
