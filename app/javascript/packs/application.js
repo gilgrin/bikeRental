5a1e57346d2f4f41084e663416a1b44e39b74374
Gil Grin <gilgi@mta.ac.il>
2020-05-20 13:20:35 +0300

    fixed navbar, heights and more

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,16 +24,10 @@ require("channels")
 
 // External imports
 import "bootstrap";
-import { initUpdateNavbarOnScroll } from '../components/navbar';
 
 // Internal imports, e.g:
-// import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
-});
-
-document.addEventListener('turbolinks:load', () => {
-  initUpdateNavbarOnScroll();
 });
